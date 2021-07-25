f82c3fb76e0b8915e5d677c21ae534325585e2d3
Yusuf Bıyık <25200573+yussufbiyik@users.noreply.github.com>
2021-07-25 19:50:10 +0300

    🖊 Edit comment lines
    Changed comment lines to English from Turkish

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const config = require("./config.json");
 
 var telegramGroup = config.telegram_channel;
 
-// Make a request for a user with a given ID
 axios.get(telegramGroup)
 	.then(async function (response) {
 		// Success ✔
-		let responseSplit = Array.from(response.data.split(" ")).reverse(); // response.data bir diziye çevirilip ters çeviriliyor ki en son link en başa gelsin,
-		let courseIndex = responseSplit.findIndex(word => word.includes("https://www.udemy.com/course/") && word.includes("?couponCode=")); // Dizinin ters çevirilmesi sayesinde arama parametrelerine uyan son linkin indexi alınıyor.
+		let responseSplit = Array.from(response.data.split(" ")).reverse(); // response.data is reversed so the latest link shows up first ,
+		let courseIndex = responseSplit.findIndex(word => word.includes("https://www.udemy.com/course/") && word.includes("?couponCode=")); // Get index of the first course link that shows up (latest one).
 		let courseInResponse = responseSplit[courseIndex]
-		let courseLink = courseInResponse.substring(6, courseInResponse.length -3);
+		let courseLink = courseInResponse.substring(6, courseInResponse.length -3); // Delete HTML tags
 		
 		let coursePage = await axios.get(courseLink)
-		let courseHeaderIndex = Array.from(coursePage.data.split("<")).findIndex(word => word.includes("udlite-heading-xl clp-lead__title clp-lead__title--small"));
+		let courseHeaderIndex = Array.from(coursePage.data.split("<")).findIndex(word => word.includes("udlite-heading-xl clp-lead__title clp-lead__title--small")); // Get the index of course title
 		let course = {
-			name: Array.from(coursePage.data.split("<"))[courseHeaderIndex].split("\n")[1],
+			name: Array.from(coursePage.data.split("<"))[courseHeaderIndex].split("\n")[1], // Get course title
 			url:courseLink,
 		}
 		console.log(course); 
