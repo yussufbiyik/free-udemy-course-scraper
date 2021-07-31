ef9f76f6a18cee175142c5280f0afda196a59ac6
Yusuf Bıyık <25200573+yussufbiyik@users.noreply.github.com>
2021-07-31 14:39:49 +0300

    Add multiple link scraping & use regex

--- index.js
+++ index.js
@@ -2,28 +2,72 @@ const axios = require('axios').default;
 const config = require("./config.json");
 
 var telegramGroup = config.telegram_channel;
+var regex = /https?:\/\/(www.)[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)[/?](couponCode=)[A-Z]\w+/g
+
+async function getLatestSingleCourse(response){
+	// Match course links
+	let courseLinks = String(response.data).match(regex);
+	// Get latest course
+	let latestCourse = courseLinks.pop()
+	// Get course title
+	let courseTitle = axios.get(latestCourse).then(response => {
+		return response.data.split("<").find(word => word.includes("udlite-heading-xl clp-lead__title clp-lead__title--small")).split("\n")[1]
+	})
+	// Return course data
+	return {
+		name: await courseTitle,
+		link: latestCourse,
+		from: config.telegram_channel
+	}
+}
+
+async function getAllCoursesFromOneChannel(response){
+	// Match course links
+	let courseLinks = String(response.data).match(regex);
+	// Delete duplicate links
+	courseLinks.filter(subject => {
+		while(courseLinks.filter(link => link === subject).length != 1){
+			courseLinks.splice(courseLinks.findIndex(link => link === subject),1)
+		}
+	})
+	let allCourses = [];
+	// Make a request for each link in courseLinks to get course titles
+	courseLinks.forEach(link => {
+		axios.get(link).then(function (result) {
+			// Push course data to allCourses
+			allCourses.push({
+				courseName: result.data.split("<").find(word => word.includes("udlite-heading-xl clp-lead__title clp-lead__title--small")).split("\n")[1],
+				courseLink: link
+			});
+		}).catch(err => console.log(err))
+	})
+	return {
+		results: allCourses,
+		from: config.telegram_channel
+	};
+}
 
 axios.get(telegramGroup)
 	.then(async function (response) {
 		// Success ✔
-		let responseSplit = Array.from(response.data.split(" ")).reverse(); // response.data is reversed so the latest link shows up first ,
-		let courseIndex = responseSplit.findIndex(word => word.includes("https://www.udemy.com/course/") && word.includes("?couponCode=")); // Get index of the first course link that shows up (latest one).
-		let courseInResponse = responseSplit[courseIndex]
-		let courseLink = courseInResponse.substring(6, courseInResponse.length -3); // Delete HTML tags
-		
-		let coursePage = await axios.get(courseLink)
-		let courseHeaderIndex = Array.from(coursePage.data.split("<")).findIndex(word => word.includes("udlite-heading-xl clp-lead__title clp-lead__title--small")); // Get the index of course title
-		let course = {
-			name: Array.from(coursePage.data.split("<"))[courseHeaderIndex].split("\n")[1], // Get course title
-			url:courseLink,
+		switch(config.mode){
+			default:
+				console.log("Please use a valid mode (single | all-from-one)")
+			case "single":
+				getLatestSingleCourse(response).then(course => console.log(course));
+				break;
+			case "all-from-one":
+				getAllCoursesFromOneChannel(response).then(courses => {
+					// wait for allCourses to be listed
+					setTimeout(() => {
+						console.log(courses)
+					}, 5000);
+				})
+				break;
 		}
-		console.log(course); 
+
 	})
 	.catch(function (error) {
 		// Fail ✖
 		console.log(error);
 	})
-	.then(function () {
-		// always executed 
-		console.log("Finished")
-	});
