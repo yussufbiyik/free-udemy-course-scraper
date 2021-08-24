ce4c36713e589d9b9a83a9b9c5e2aa2ec8b0056b
Yusuf Bıyık <25200573+yussufbiyik@users.noreply.github.com>
2021-08-25 01:59:40 +0300

    🧰 Use regex for detecting course names and more!
    Changes:
    + Use regex for detecting course names, line breaks, html entities
    + Use try catch to log possible errors
    + Fix a problem where invalid courses cause the script to give an error

--- index.js
+++ index.js
@@ -2,71 +2,96 @@ const axios = require('axios').default;
 const config = require("./config.json");
 
 var telegramGroup = config.telegram_channel;
-var regex = /https?:\/\/(www.)[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)[/?](couponCode=)[A-Z]\w+/g
+var regexForCourseLinkDetection = /https?:\/\/(www.)[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)[/?](couponCode=)[A-Z]\w+/g
+var regexForCourseNameDetection = /(?<=(data-purpose="lead-title">))(\w|\d|\n|[().,\-:;@#$%^&*\[\]"'+–/\/®°⁰!?{}|`~]| )+?(?=(<\/h1>))/g
+var regexForHTMLEntitiyDetection = /(&.+;)/ig;
+var regexForLineBreakDetection = /(\n)/g;
 
-async function getLatestSingleCourse(response){
-	// Match course links
-	let courseLinks = String(response.data).match(regex);
-	// Get latest course
-	let latestCourse = courseLinks.pop()
-	// Get course title
-	let courseTitle = axios.get(latestCourse).then(response => {
-		return response.data.split("<").find(word => word.includes("udlite-heading-xl clp-lead__title clp-lead__title--small")).split("\n")[1]
-	})
-	// Return course data
-	return {
-		name: await courseTitle,
-		link: latestCourse,
-		from: config.telegram_channel
-	}
-}
-
-async function getAllCoursesFromOneChannel(response){
-	// Match course links
-	let courseLinks = String(response.data).match(regex);
-	// Delete duplicate links
-	courseLinks.filter(subject => {
-		while(courseLinks.filter(link => link === subject).length != 1){
-			courseLinks.splice(courseLinks.findIndex(link => link === subject),1)
+async function getLatestSingleCourse(response) {
+	try {
+		// Match course links
+		let courseLinks = String(response.data).match(regexForCourseLinkDetection);
+		// Get latest course
+		let latestCourse = courseLinks.pop()
+		// Check whether or not the latest course name is null, if so the course is invalid
+		if (String(response.data).match(regexForCourseNameDetection) == null) {
+			return `Latest Course is Invalid, Course link was: ${latestCourse}`
+		} else {
+			// Get course title
+			let courseTitle = axios.get(latestCourse).then(response => {
+				return String(response.data).match(regexForCourseNameDetection).join(' ').replace(regexForHTMLEntitiyDetection, '').replace(regexForLineBreakDetection, '')
+			})
+			// Return course data
+			return {
+				name: await courseTitle,
+				link: latestCourse,
+				from: config.telegram_channel
+			}
 		}
-	})
-	let allCourses = [];
-	// Make a request for each link in courseLinks to get course titles
-	for await (let link of courseLinks){
-		await axios.get(link).then(function (result) {
-			// Push course data to allCourses
-			allCourses.push({
-				courseName: result.data.split("<").find(word => word.includes("udlite-heading-xl clp-lead__title clp-lead__title--small")).split("\n")[1],
-				courseLink: link
-			});
-		}).catch(err => console.log(err))
+	} catch (error) {
+		console.error(error)
 	}
-	return {
-		results: allCourses,
-		from: config.telegram_channel
-	};
 }
 
-if(config.mode === "all-from-one" || config.mode === "single"){
+async function getAllCoursesFromOneChannel(response) {
+	try {
+		// Match course links
+		let courseLinks = String(response.data).match(regexForCourseLinkDetection);
+		// Delete duplicate links
+		courseLinks.filter(subject => {
+			while (courseLinks.filter(link => link === subject).length != 1) {
+				courseLinks.splice(courseLinks.findIndex(link => link === subject), 1)
+			}
+		})
+		let allCourses = [];
+		// Make a request for each link in courseLinks to get course titles
+		for await (let link of courseLinks) {
+			await axios.get(link).then(function (result) {
+				// Check whether or not the latest course name is null, if so the course is invalid
+				if (String(result.data).match(regexForCourseNameDetection) == null) {
+					allCourses.push({
+						courseName: 'invalid course',
+						courseLink: link
+					})
+				} else {
+					// Push course data to allCourses
+					let courseName = String(result.data).match(regexForCourseNameDetection).join(' ').replace(regexForHTMLEntitiyDetection, '').replace(regexForLineBreakDetection, '');
+					allCourses.push({
+						courseName: courseName,
+						courseLink: link
+					});
+				}
+			})
+		}
+		return {
+			results: allCourses,
+			from: config.telegram_channel
+		};
+	} catch (error) {
+		console.error(error)
+	}
+}
+
+if (config.mode === "all-from-one" || config.mode === "single") {
 	axios.get(telegramGroup)
-	.then(async function (response) {
-		// Success ✔
-		switch(config.mode){
-			case "single":
-				getLatestSingleCourse(response).then(course => console.log(course));
-				break;
-			case "all-from-one":
-				getAllCoursesFromOneChannel(response).then(courses => console.log(courses))
-				break;
-			default:
-				console.log("Please use a valid mode (single | all-from-one)")
-		}
+		.then(async function (response) {
+			// Success ✔
+			switch (config.mode) {
+				case "single":
+					getLatestSingleCourse(response).then(course => console.log(course));
+					break;
+				case "all-from-one":
+					getAllCoursesFromOneChannel(response).then(courses => console.log(courses))
+					break;
+				default:
+					console.log("Please use a valid mode (single | all-from-one)")
+			}
 
-	})
-	.catch(function (error) {
-		// Fail ✖
-		console.log(error);
-	})
-}else{
+		})
+		.catch(function (error) {
+			// Fail ✖
+			console.log(error);
+		})
+} else {
 	console.log("Please use a valid mode (modes: single || all-from-one)")
 }
